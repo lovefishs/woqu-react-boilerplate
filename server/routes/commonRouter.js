9cd7d1a9446560fcccbf817cb5c815d624e3d108
changming.huang <changming.huang@woqutech.com>
2017-08-09 15:57:38 +0800

    env: add mockjs module

--- server/routes/commonRouter.js
+++ server/routes/commonRouter.js
@@ -1,4 +1,3 @@
-// const Mock = require('mockjs')
 const sleep = require('../utils/sleep')
 
 module.exports = (router, app) => {
@@ -7,27 +6,19 @@ module.exports = (router, app) => {
     .use(router.allowedMethods())
 
 
-  // 用户登录
-  router.get('/user/:userId', async (ctx, next) => {
+  // get user information
+  router.get('/user/:id(\\d+)', async (ctx, next) => {
     // const query = ctx.query
     const params = ctx.params
     // const data = ctx.request.body
-    const result = {
+    const data = require('./data/common/user_id_get').build(params.id)
+
+    await sleep(500)
+
+    ctx.body = {
+      data,
       message: '',
       error_code: 0,
-      data: {
-        id: params.userId,
-        name: 'admin',
-        group: 'default',
-        email: 'mail@woqutech.com',
-        mobile: '152xxxxxxxx',
-        create_time: '2016-12-28 05:19:06',
-        token: 'xxx-yyy-zzz',
-      },
     }
-
-    await sleep(1000)
-
-    ctx.body = result
   })
 }
---
+++ server/routes/data/common/user_id_get.js
@@ -0,0 +1,21 @@
+const Mock = require('mockjs')
+
+class DataCenter {
+  constructor () {}
+
+  build (id = 0) {
+    return Mock.mock({
+      data: {
+        id: id,
+        name: 'admin',
+        group: 'default',
+        email: 'mail@woqutech.com',
+        'mobile|1': ['15258888888', '15259999999'],
+        create_time: new Date(),
+        token: /[a-z]{3}-[A-Z]{3}-[0-9]{3}/,
+      },
+    })
+  }
+}
+
+module.exports = new DataCenter()
