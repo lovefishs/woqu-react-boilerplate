dfb27a1ed47d7924abfba339e5ed23c23301d615
changming.huang <changming.huang@woqutech.com>
2017-09-15 15:39:59 +0800

    env: update mock data

--- server/routes/data/common/user_id_get.js
+++ server/routes/data/common/user_id_get.js
@@ -5,15 +5,13 @@ class DataCenter {
 
   build (id = 0) {
     return Mock.mock({
-      data: {
-        id: parseInt(id, 10),
-        name: 'admin',
-        group: 'default',
-        email: 'mail@woqutech.com',
-        'mobile|1': ['15258888888', '15259999999'],
-        create_time: new Date(),
-        token: /[a-z]{3}-[A-Z]{3}-[0-9]{3}/,
-      },
+      id: parseInt(id, 10),
+      name: 'admin',
+      group: 'default',
+      email: 'mail@woqutech.com',
+      'mobile|1': ['15258888888', '15259999999'],
+      create_time: new Date(),
+      token: /[a-z]{3}-[A-Z]{3}-[0-9]{3}/,
     })
   }
 }
